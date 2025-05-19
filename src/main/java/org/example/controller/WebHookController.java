46a12fffe90add5d8502cfe2f2e3087fcf85f916
Ivan Karmanov <van.karmanov@yandex.by>
2025-05-19 14:53:45 +0300

    fix

--- src/main/java/org/example/controller/WebHookController.java
+++ src/main/java/org/example/controller/WebHookController.java
@@ -1,10 +1,7 @@
 package org.example.controller;
 
 import dev.karmanov.library.service.botCommand.BotHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
@@ -20,5 +17,11 @@ public class WebHookController {
     public void getUpdate(@RequestBody Update update){
         System.out.println(update);
         botHandler.handleMessage(update);
+    }
+
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK";
     }
 }
